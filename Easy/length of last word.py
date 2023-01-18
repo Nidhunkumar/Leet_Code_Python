60ed0c305170b2e01dbb26defa9de85771b0f4a4
Nidhunkumar <nidhunkumarap@gmail.com>
2023-01-18 21:56:44 +0530

    plus one

--- Easy/length of last word.py
+++ Easy/length of last word.py
@@ -23,3 +23,21 @@ Input: s = "luffy is still joyboy"
 Output: 6
 Explanation: The last word is "joyboy" with length 6.
 '''
+s="luffy is still joyboy"
+
+less time
+s = list(s.rstrip().split())
+print(len(s[-1]))
+# less memory
+ for i in range(len(s.split())):
+            x1 = max(0, len(s.split()[i]) )
+        return x1
+
+# for word in s.split(' ')[::-1]:
+#             if word != '':
+#                 print(len(word))
+# s=string.split()
+# print(s)
+# last_word=s[len(s)-1]
+# print(last_word)
+# print(len(last_word))
