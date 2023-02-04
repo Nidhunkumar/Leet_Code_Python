91fadf6c52012a8e05a03f9b5e4e4aff5f52e2f9
Nidhunkumar <nidhunkumarap@gmail.com>
2023-02-04 22:20:46 +0530

    symmetric tree (completed)

--- Easy/symmetric tree.py
+++ Easy/symmetric tree.py
@@ -14,3 +14,12 @@ Example 2:
 Input: root = [1,2,2,null,3,null,3]
 Output: false
 '''
+ if leftRoot is None and rightRoot is None:
+            return True
+        if (leftRoot is None and rightRoot is not None) or (leftRoot is not None and rightRoot is None):
+            return False
+        if leftRoot.val != rightRoot.val:
+            return False
+        return self.isTreeSymmetric(leftRoot.left, rightRoot.right) and self.isTreeSymmetric(leftRoot.right, rightRoot.left)
+    def isSymmetric(self, root):
+        return self.isTreeSymmetric(root.left, root.right)
