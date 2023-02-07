e51967f893a72e3d772225b07b272ccbb82bf6ae
Nidhunkumar <nidhunkumarap@gmail.com>
2023-02-07 21:12:05 +0530

    balanced binary tree

--- Easy/convert sorted array into binary search tree.py
+++ Easy/convert sorted array into binary search tree.py
@@ -17,3 +17,30 @@ Example 2:
 Input: nums = [1,3]
 Output: [3,1]
 Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.'''
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        total_nums = len(nums)
+        if not total_nums:
+            return None
+        mid_node = total_nums // 2
+        return TreeNode(nums[mid_node],self.sortedArrayToBST(nums[:mid_node]), self.sortedArrayToBST(nums[mid_node + 1 :]))
+    
+    #less time
+    class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        size = len(nums)
+        #if size == 1:
+        #    return nums[size-1]
+
+        def convert(left, right):
+            if left > right:
+                return None
+            mid = (left + right) // 2
+
+            # process left
+            left = convert(left, mid - 1)
+            node = TreeNode(nums[mid])
+            node.left = left
+            node.right = convert(mid+1, right)
+            return node
+        return convert(0, size-1)
