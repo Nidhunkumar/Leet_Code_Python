b329a6887fdfad876fd002fd253748605a29b7e8
Nidhunkumar <nidhunkumarap@gmail.com>
2023-02-08 20:48:00 +0530

    minimum depth of binary tree

--- Easy/balanced binary tree.py
+++ Easy/balanced binary tree.py
@@ -17,3 +17,31 @@ Example 3:
 Input: root = []
 Output: true
 '''
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+           return (self.Height(root) >= 0)
+    def Height(self, root):
+        if root is None:  return 0
+        leftheight, rightheight = self.Height(root.left), self.Height(root.right)
+        if leftheight < 0 or rightheight < 0 or abs(leftheight - rightheight) > 1:  return -1
+        return max(leftheight, rightheight) + 1
+    
+#less time
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def sample(root):
+            if not root:
+                return 0
+            lh = sample(root.left)
+            if lh == -1:
+                return -1
+            rh = sample(root.right)
+            if rh == -1:
+                return -1
+            if abs(lh-rh)>1:
+                return -1
+            else: 
+                return 1+max(lh,rh)
+            
+        return sample(root)!=-1
