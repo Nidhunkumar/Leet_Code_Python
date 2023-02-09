866677212337ef77605d44e681c25f960ea45a37
Nidhunkumar <nidhunkumarap@gmail.com>
2023-02-09 20:45:46 +0530

    minimum depth of binary tree

--- Easy/minimum depth of binary tree.py
+++ Easy/minimum depth of binary tree.py
@@ -16,3 +16,39 @@ Example 2:
 Input: root = [2,null,3,null,4,null,5,null,6]
 Output: 5
 '''
+# Base case...
+        # If the subtree is empty i.e. root is NULL, return depth as 0...
+        if root is None:  return 0
+        # Initialize the depth of two subtrees...
+        leftDepth = self.minDepth(root.left)
+        rightDepth = self.minDepth(root.right)
+        # If the both subtrees are empty...
+        if root.left is None and root.right is None:
+            return 1
+        # If the left subtree is empty, return the depth of right subtree after adding 1 to it...
+        if root.left is None:
+            return 1 + rightDepth
+        # If the right subtree is empty, return the depth of left subtree after adding 1 to it...
+        if root.right is None:
+            return 1 + leftDepth
+        # When the two child function return its depth...
+        # Pick the minimum out of these two subtrees and return this value after adding 1 to it...
+        return min(leftDepth, rightDepth) + 1;    # Adding 1 is the current node which is the parent of the two subtrees...
+
+#less time
+ if root is None:
+            return 0
+        cur = [root]
+        lvl = 0
+        while cur:
+            lvl += 1
+            nextlvl = []
+            for node in cur:
+                if node.left:
+                    nextlvl.append(node.left)
+                if node.right:
+                    nextlvl.append(node.right)
+                if not node.left and not node.right:
+                    return lvl
+            cur = nextlvl
+  
