e80cf2fa37335ba9362d21d840aa2de4911bb92a
Nidhunkumar <nidhunkumarap@gmail.com>
2023-02-15 21:53:44 +0530

    online status

---
+++ Others/Easy/online status.py
@@ -0,0 +1,25 @@
+'''Challenge
+Online status
+The aim of this challenge is, given a dictionary of people's online status, to count the number of people who are online.
+
+For example, consider the following dictionary:
+
+statuses = {
+    "Alice": "online",
+    "Bob": "offline",
+    "Eve": "online",
+}
+In this case, the number of people online is 2.
+
+Write a function named online_count that takes one parameter. The parameter is a dictionary that maps from strings of names to the string "online" or "offline", as seen above.
+
+Your function should return the number of people who are online.
+'''
+statuses = {
+    "Alice": "online",
+    "Bob": "offline",
+    "Eve": "online",
+}
+def online_count(status): 
+    return len([i for i in status if status[i]=="online"])
+print(online_count(statuses))
