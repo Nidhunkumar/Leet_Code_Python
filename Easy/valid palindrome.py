6d811ccb5adb422cbdb9dcff7dc056c647ce1a23
Nidhunkumar <nidhunkumarap@gmail.com>
2023-02-19 22:11:29 +0530

    single number

--- Easy/valid palindrome.py
+++ Easy/valid palindrome.py
@@ -22,3 +22,16 @@ Output: true
 Explanation: s is an empty string "" after removing non-alphanumeric characters.
 Since an empty string reads the same forward and backward, it is a palindrome.
 '''
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # Convert to lowercase and remove non-alphanumeric characters
+        s = ''.join(c for c in s if c.isalnum()).lower()
+        # Check if string is equal to its reverse
+        return s == s[::-1]
+    
+#less time
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        forward_striped_s = [c for c in s.lower() if c.isalnum()]
+        backward_striped_s = forward_striped_s[::-1]
+        return forward_striped_s == backward_striped_s
