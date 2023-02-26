cb141967518aae77afea4d6bc9f217e3e5a571d5
Nidhunkumar <nidhunkumarap@gmail.com>
2023-02-26 22:10:50 +0530

    min maxing

--- Others/Easy/Flatten a list.py
+++ Others/Easy/Flatten a list.py
@@ -12,3 +12,19 @@ Should return the list:
 [1, 2, 3, 4]
 '''
 
+# naive solution
+def flatten(outer_list):
+    result = []
+    for inner_list in outer_list:
+        for item in inner_list:
+            result.append(item)
+    return result
+
+# solution with nested list comprehensions
+# (can be put on a single line for conciseness)
+def flatten(outer_list):
+    return [
+        item
+        for inner_list in outer_list
+        for item in inner_list
+    ]
