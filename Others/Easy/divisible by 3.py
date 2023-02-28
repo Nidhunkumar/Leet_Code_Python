92cd450754fa1dd969a5c5b0cc06c55c0c24f857
Nidhunkumar <nidhunkumarap@gmail.com>
2023-02-28 21:19:43 +0530

    tic tac toe input

--- Others/Easy/divisible by 3.py
+++ Others/Easy/divisible by 3.py
@@ -4,5 +4,5 @@ Define a function named div_3 that returns True if its single integer parameter 
 For example, div_3(6) is True because 6/3 does not leave any remainder. However div_3(5) is False because 5/3 leaves 2 as a remainder.
 
 '''
-# def div_3(a):
-#     return a % 3 == 0
+def div_3(a):
+    return a % 3 == 0
