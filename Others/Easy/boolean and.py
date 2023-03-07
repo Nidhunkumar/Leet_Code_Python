75515fb4751e785a829a4ae1250cf0bba7031b1d
Nidhunkumar <nidhunkumarap@gmail.com>
2023-03-07 19:56:06 +0530

    boolean and

---
+++ Others/Easy/boolean and.py
@@ -0,0 +1,4 @@
+'''
+Boolean and
+Define a function named triple_and that takes three parameters and returns True only if they are all True and False otherwise.
+'''
--- Others/Easy/consecutive zeros.py
+++ Others/Easy/consecutive zeros.py
@@ -5,3 +5,30 @@ The goal of this challenge is to analyze a binary string consisting of only zero
 The biggest number of consecutive zeros is 3.
 
 Define a function named consecutive_zeros that takes a single parameter, which is the string of zeros and ones. Your function should return the number described above.'''
+
+a="1001101000110"
+l=a.split("1")
+#print(l)
+mx=l[0]
+for i in range(len(a.split("1"))):
+    if mx< l[i]:
+        mx =l[i]
+print(len(mx))     
+        
+# naive solution
+def consecutive_zeros(bin_str):
+    result = 0
+    streak = 0
+    for letter in bin_str:
+        if letter == "0":
+            streak += 1
+        else:
+            streak = 0
+        result = max(result, streak)
+    return result
+
+# shorter solution
+def consecutive_zeros(bin_str):
+    return max([len(s) for s in bin_str.split("1")])
+    
+     
