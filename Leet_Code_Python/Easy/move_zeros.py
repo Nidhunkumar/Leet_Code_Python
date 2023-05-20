92e8168b8271e340f951dc6dba7218d4b8d97f2c
Nidhunkumar <nidhunkumarap@gmail.com>
2023-05-20 21:09:46 +0530

    word pattern

--- Leet_Code_Python/Easy/move_zeros.py
+++ Leet_Code_Python/Easy/move_zeros.py
@@ -20,3 +20,52 @@ Input: nums = [0]
 Output: [0]
 
 '''
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)):
+            if nums[i]==0:
+                nums.append(0)
+                nums.remove(0)
+
+#less time
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        insertIndex = 0
+        length = len(nums)
+        for i in range(length):
+            if nums[i] != 0:
+                nums[insertIndex] = nums[i]
+                insertIndex+=1
+        while insertIndex < length:
+            nums[insertIndex] = 0
+            insertIndex+=1
+
+#less memory
+#[0,1,0,3,12]
+# 1 0
+#[1,0,0,3,12]
+#[1,3,0,0,12]
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        lp, rp, len_n = 0, 0, len(nums)
+        while rp < len_n:
+            while lp < len_n and nums[lp] != 0:
+                lp += 1
+                
+            rp = lp + 1
+            while rp < len_n and nums[rp] == 0:
+                rp += 1
+                
+            if lp < len_n and rp < len_n and lp != rp:
+                nums[lp], nums[rp] = nums[rp], nums[lp]
+                lp += 1
+        
