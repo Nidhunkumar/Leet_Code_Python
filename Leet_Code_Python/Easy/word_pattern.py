a53840a3495d2217c81e08945f2b9c62a5eef198
Nidhunkumar <nidhunkumarap@gmail.com>
2023-05-22 22:20:38 +0530

    nim game

--- Leet_Code_Python/Easy/word_pattern.py
+++ Leet_Code_Python/Easy/word_pattern.py
@@ -20,3 +20,56 @@ Input: pattern = "aaaa", s = "dog cat cat dog"
 Output: false
 
 '''
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        s = s.split()
+        n = len(pattern)
+        m = len(s)
+        if n!=m:
+            return False
+        dic = {}
+        for i,j in zip(pattern,s):
+            if (i in dic and j!=dic[i]):
+                return False
+            dic[i] = j
+            if len(set(dic.values()))!=len(set(dic.keys())):
+                return False
+        return True
+
+#less time
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split(" ")
+        if len(pattern) != len(words):
+            return False
+        charToWord = {}
+        wordToChar = {}
+
+        for c,w in zip(pattern, words):
+            if c in charToWord and charToWord[c] != w:
+                return False
+            if w in wordToChar and wordToChar[w] != c:
+                return False
+
+            charToWord[c] = w
+            wordToChar[w] = c
+
+        return True
+
+#less memory
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split(" ")
+        if len(pattern) != len(words):
+            return False
+
+        toword = {}
+        tochar = {}
+        for x,y in zip(pattern,words):
+          if x in toword and toword[x] != y:
+            return False
+          if y in tochar and tochar[y] != x:
+            return False
+          toword[x] = y
+          tochar[y] = x
+        return True
