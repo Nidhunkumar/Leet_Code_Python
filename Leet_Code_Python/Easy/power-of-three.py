f6e35a19fa4faff0ae723aaf187170cd33464998
Nidhunkumar <nidhunkumarap@gmail.com>
2023-05-26 22:48:02 +0530

    counting bits

--- Leet_Code_Python/Easy/power-of-three.py
+++ Leet_Code_Python/Easy/power-of-three.py
@@ -24,3 +24,28 @@ Output: false
 Explanation: There is no x where 3x = (-1).
 
 '''
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        return (n > 0) and 1162261467 % n == 0
+
+#less time
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        while True:
+            if n == 1:
+                return True
+            if n % 3 != 0:
+                return False
+            n = n // 3
+        return False
+
+#less memory
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        while n % 3 == 0:
+            n = n / 3
+        return n == 1
