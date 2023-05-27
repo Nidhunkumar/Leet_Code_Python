e1a9727a50738a7e9083b657ef1714c6f173bc42
Nidhunkumar <nidhunkumarap@gmail.com>
2023-05-27 22:13:21 +0530

    power of four

--- Leet_Code_Python/Easy/counting_bits.py
+++ Leet_Code_Python/Easy/counting_bits.py
@@ -29,3 +29,28 @@ Explanation:
 5 --> 101
 
 '''
+
+class Solution:
+    def countBits(self, n: int) -> Iterable[int]:
+        return map(int.bit_count, range(n + 1))
+
+#less time   
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        res = [0]
+        offset = 1
+        for i in range(1, n + 1):
+            if offset*2 == i:
+                offset *= 2
+            res.append(1 + res[i - offset])
+        return res
+  
+#less memory
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        return map(lambda x: list(reversed(bin(x)[2:])).count('1'), range(0, n + 1))
+    
+    
+    
