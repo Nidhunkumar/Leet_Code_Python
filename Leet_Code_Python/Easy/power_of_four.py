e08a02ff9cefb949e39451815b3a7cda2acaa9a3
Nidhunkumar <nidhunkumarap@gmail.com>
2023-05-28 22:11:02 +0530

    reverse string

--- Leet_Code_Python/Easy/power_of_four.py
+++ Leet_Code_Python/Easy/power_of_four.py
@@ -21,3 +21,27 @@ Input: n = 1
 Output: true
 
 '''
+
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n==1:
+            return True
+        if n==0 or n%4!=0:
+            return False
+        return Solution.isPowerOfFour(self,n/4)
+    
+#less time
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        tmp = 1
+        while tmp < n:
+            tmp *= 4
+        return tmp == n    
+  
+#less memory
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        step=1
+        while step<n:
+            step*=4
+        return n==step
