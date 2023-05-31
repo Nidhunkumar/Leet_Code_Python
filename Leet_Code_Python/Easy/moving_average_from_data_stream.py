558271ede4c10cf25803b4a3a4f599414a3981af
Nidhunkumar <nidhunkumarap@gmail.com>
2023-05-31 23:00:40 +0530

    moving average from data stream

---
+++ Leet_Code_Python/Easy/moving_average_from_data_stream.py
@@ -0,0 +1,16 @@
+'''
+346.moving average from data stream
+
+Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
+
+Sample I/O
+Example 1
+MovingAverage m = new MovingAverage(3);
+m.next(1) = 1
+m.next(10) = (1 + 10) / 2
+m.next(3) = (1 + 10 + 3) / 3
+m.next(5) = (10 + 3 + 5) / 3
+
+
+
+'''
--- Leet_Code_Python/Easy/reverse_vowels_of_a_string.py
+++ Leet_Code_Python/Easy/reverse_vowels_of_a_string.py
@@ -17,3 +17,51 @@ Input: s = "leetcode"
 Output: "leotcede"
 
 '''
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        loc=[]
+        s=list(s)
+        for i in range(len(s)):
+            if(s[i] in "aeiouAEIOU"):
+                loc.append(i)
+        for i in range(len(loc)//2):
+            s[loc[i]],s[loc[-i-1]]=s[loc[-i-1]],s[loc[i]]
+        return "".join(s)
+
+#less time
+
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        word = list(s)
+        vowels = set("aeiouAEIOU")
+
+        l, r = 0, len(word)-1
+
+        while l <= r:
+            while l <= r and word[l] not in vowels: l += 1
+            while l <= r and word[r] not in vowels: r -=1
+            
+            if l > r: break
+            word[l], word[r] = word[r], word[l]
+            l += 1
+            r -= 1
+        return "".join(word)
+
+#less memory
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        vowel_str=""
+        output=""
+        for i in s:
+            if i in "aeiouAEIOU":
+                vowel_str+=i
+        rev_vowel=vowel_str[::-1]
+        x=0
+        for i in s:
+            if i in "aeiouAEIOU":
+                output+=rev_vowel[x]
+                x+=1
+            else:
+                output+=i
+        return output
+
