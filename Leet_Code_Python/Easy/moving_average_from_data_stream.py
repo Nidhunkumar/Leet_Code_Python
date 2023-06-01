1edd5e0267a0203eb3d250d2cf1c28a748ab1cea
Nidhunkumar <nidhunkumarap@gmail.com>
2023-06-01 22:28:23 +0530

    Intersection of Two Arrays

--- Leet_Code_Python/Easy/moving_average_from_data_stream.py
+++ Leet_Code_Python/Easy/moving_average_from_data_stream.py
@@ -14,3 +14,21 @@ m.next(5) = (10 + 3 + 5) / 3
 
 
 '''
+import collections
+class MovingAverage:
+
+    def __init__(self, size: int):
+        """
+        Initialize your data structure here.
+        """
+        self.queue = collections.deque()
+        self.size = size
+
+    def next(self, val: int) -> float:
+        if len(self.queue) == self.size:
+            self.queue.popleft()
+            self.queue.append(val)
+        else:
+            self.queue.append(val)
+        return sum(self.queue)/len(self.queue)
+    
