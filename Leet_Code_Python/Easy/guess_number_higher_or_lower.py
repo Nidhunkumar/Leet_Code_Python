2e9b4595a75e6d0bf97c1c2b489ecd924f0fc3cb
Nidhunkumar <nidhunkumarap@gmail.com>
2023-06-06 21:17:12 +0530

    Ransom Note

--- Leet_Code_Python/Easy/guess_number_higher_or_lower.py
+++ Leet_Code_Python/Easy/guess_number_higher_or_lower.py
@@ -30,3 +30,76 @@ Input: n = 2, pick = 1
 Output: 1
 
 '''
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        l, r = 1, n
+        while l < r:
+            pass
+def guess (n):
+    """
+    :type n: int
+    :rtype: int
+    """
+    m=6
+    if n > m:
+        return -1
+    elif n<m:
+        return 1
+    elif n==m:
+        return 0
+
+
+def guessNumber( n: int) -> int:
+    l,r=1,n
+    while l<r:
+        mid=l+(r-l)//2
+        if guess(mid)==0:
+            return mid
+        elif guess(mid)==-1:
+            r=mid-1
+        elif guess(mid)==1:
+            l=mid+1
+
+
+print(guessNumber(10))
+
+#less time
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        left, right = 1, n
+        while left < right:
+            mid = (left + right)//2
+            if guess(mid) < 1:
+                right = mid
+            else:
+                left = mid + 1
+        return left
+
+#less memory
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        l = 1
+        r = n
+        while l<=r:
+            mid = (l+r)//2
+            g = guess(mid)
+            if g == 0:
+                return mid
+            if g==1:
+                l = mid+1
+            else:
+                r=mid-1
+        return l
+
