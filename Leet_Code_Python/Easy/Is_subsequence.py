44c72a06c4996e055c2e986f9def2bb52c3d843b
Nidhunkumar <nidhunkumarap@gmail.com>
2023-06-10 13:25:47 +0530

    Binary Watch

--- Leet_Code_Python/Easy/Is_subsequence.py
+++ Leet_Code_Python/Easy/Is_subsequence.py
@@ -15,3 +15,51 @@ Input: s = "axc", t = "ahbgdc"
 Output: false
 
 '''
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if len(s) > len(t):return False
+        if len(s) == 0:return True
+        subsequence=0
+        for i in range(0,len(t)):
+            if subsequence <= len(s) -1:
+                print(s[subsequence])
+                if s[subsequence]==t[i]:
+
+                    subsequence+=1
+        return  subsequence == len(s) 
+    
+#less time
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if len(s) > len(t):
+            return False
+        
+        i, j = 0, 0
+
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+                j += 1
+            else:
+                j += 1
+        
+        return True if i == len(s) else False
+
+#less memory
+class Solution:
+    def isSubsequence(self, s, t):
+        lst1 =[]
+        i = 0
+        j = 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                lst1.append(s[i])
+                i += 1
+                j += 1
+            else:
+                j += 1
+        return len(lst1) == len(s)
+    
+
+    
