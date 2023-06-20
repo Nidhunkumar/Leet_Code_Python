da9b5def96317b0b0237128088e070708b1fc49a
Nidhunkumar <nidhunkumarap@gmail.com>
2023-06-20 19:17:43 +0530

    Arranging Coins

--- Leet_Code_Python/Easy/Number_of_segments_in_a_string.py
+++ Leet_Code_Python/Easy/Number_of_segments_in_a_string.py
@@ -18,3 +18,38 @@ Input: s = "Hello"
 Output: 1
 
 '''
+class Solution:
+    def countSegments(self, s: str) -> int:
+        return len(s.split())
+
+#less time
+class Solution:
+    def countSegments(self, s: str) -> int:
+        s = s.strip()
+
+        if len(s) == 0:
+            return 0
+
+        spaces = 0
+        for i, char in enumerate(s):
+            if s[i] == ' ' and i < len(s)-1 and s[i+1] != ' ':
+                spaces += 1
+        return spaces + 1
+
+#less memory
+class Solution:
+    def countSegments(self, s: str) -> int:
+        c=1
+        d=""
+        a=[]
+        for i in range(len(s)):
+            print(s[i])
+            if s[i] !=" ":
+                d+=s[i]
+            else:
+                if d!="":
+                    a.append(d)
+                d=""
+        if d!="":
+            a.append(d)
+        return(len(a))
