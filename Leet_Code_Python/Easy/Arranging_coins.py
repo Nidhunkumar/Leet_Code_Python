667f7085c88eb45a7d749994a3859d6f53526dc5
Nidhunkumar <nidhunkumarap@gmail.com>
2023-06-21 21:52:37 +0530

    Find All Numbers Disappeared in an Array

--- Leet_Code_Python/Easy/Arranging_coins.py
+++ Leet_Code_Python/Easy/Arranging_coins.py
@@ -21,3 +21,31 @@ Output: 3
 Explanation: Because the 4th row is incomplete, we return 3.
 
 '''
+import math
+
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        ans = -0.5 + math.sqrt(1+8*n)/2
+        return int(ans) 
+    
+#less time
+
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        left = 0
+        right = n
+        while left <= right:
+            middle = (left + right) // 2
+            starsNeeded = (middle*(middle+1)) // 2
+            if starsNeeded == n:
+                return middle
+            if starsNeeded > n:
+                right = middle - 1
+            else:
+                left = middle + 1
+        return right
+
+#less memory
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        return int((math.sqrt(8*n + 1) - 1) / 2)
