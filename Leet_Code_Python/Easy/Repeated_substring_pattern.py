ef3b5235768087b84992a8b730b1e21fb9dc3486
Nidhunkumar <nidhunkumarap@gmail.com>
2023-06-24 21:57:39 +0530

    Hamming Distance

--- Leet_Code_Python/Easy/Repeated_substring_pattern.py
+++ Leet_Code_Python/Easy/Repeated_substring_pattern.py
@@ -19,3 +19,25 @@ Output: true
 Explanation: It is the substring "abc" four times or the substring "abcabc" twice.
 
 '''
+class Solution:
+  def repeatedSubstringPattern(self, s: str) -> bool:
+    # calculate the length of the string
+    n = len(s)
+    # try only those substring lengths that are factors of the length of the string
+    for i in range(1, n // 2 + 1):
+        if n % i == 0:
+            # check only those substrings that start at the beginning of the string
+            if s[:i] * (n // i) == s:
+                return True
+    
+    return False
+  
+  #less time
+  class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        return s in s[1:] + s[:-1]
+    
+#less memory
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        return s in s[1:] + s[:-1]
