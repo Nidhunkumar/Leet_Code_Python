881e58d36bf22ecccd2e99e0ec6ab31f5b10efab
Nidhunkumar <nidhunkumarap@gmail.com>
2023-07-11 09:03:40 +0530

    Find Mode in Binary Search Tree

--- Leet_Code_Python/Easy/Keyboard_raw.py
+++ Leet_Code_Python/Easy/Keyboard_raw.py
@@ -28,3 +28,46 @@ Input: words = ["adsdf","sfd"]
 Output: ["adsdf","sfd"]
 
 '''
+
+rows = (set("qwertyuiop"), set("asdfghjkl"), set("zxcvbnm"))
+
+class Solution:
+    def findWords(self, words: list[str]) -> list[str]:
+        
+        return [w for w in words if any(set(w.lower()).issubset(row) for row in rows)]
+    
+#less time
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        rows = (set("qwertyuiop"),set("asdfghjkl"),set("zxcvbnm"))
+        return (w for row in rows for w in words if not set(w.lower()) - row )
+    
+#less memory
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        l1=list('qwertyuiop')
+        l2=list('asdfghjkl')
+        l3=list('zxcvbnm')
+        l=[]
+        for i in words:
+            l4=list(i)
+            c1=0
+            c2=0
+            c3=0
+            for j in i:
+                s1=j.lower() in l1
+                s2=j.lower() in l2
+                s3=j.lower() in l3
+                if(s1==True):
+                    c1+=1
+                elif(s2==True):
+                    c2+=1
+                elif(s3==True):
+                    c3+=1
+            if(c1==len(i)):
+                l.append(i)
+            elif(c2==len(i)):
+                l.append(i)
+            elif(c3==len(i)):
+                l.append(i)
+        return(l)
