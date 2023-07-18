4088dc5056140e7efc68fedcbba9d6ca13bc7860
Nidhunkumar <nidhunkumarap@gmail.com>
2023-07-18 08:51:22 +0530

    Detect Capital

--- Leet_Code_Python/Easy/Fibonacci_number.py
+++ Leet_Code_Python/Easy/Fibonacci_number.py
@@ -27,3 +27,51 @@ Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
 
 
 '''
+
+class Solution:
+    def fib(self, n: int) -> int:
+        def fib(n):
+            if Fib[n] != -1:
+                return Fib[n]
+            Fib[n] = fib(n-1) + fib(n-2)
+            return Fib[n]
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        Fib = [-1 for _ in range(n+1)]
+        Fib[0] = 0
+        Fib[1] = 1
+        return fib(n)
+    
+#less time
+
+class Solution:
+  def fib(self, n: int) -> int:
+    if n < 2:
+      return n
+
+    dp = [0, 0, 1]
+
+    for i in range(2, n + 1):
+      dp[0] = dp[1]
+      dp[1] = dp[2]
+      dp[2] = dp[0] + dp[1]
+
+    return dp[2]
+  
+#less memory
+class Solution:
+    def fib(self, n: int) -> int:
+        def helper(n, memo):
+            if n in memo:
+                return memo[n]
+            if n <= 1:
+                return n
+
+            memo[n] = helper(n - 1, memo) + helper(n - 2, memo)
+            return memo[n]
+        
+        return helper(n, {})
+
+
