5ca954db97dc86fa0ea3e90f4362f86c0856a568
Nidhunkumar <nidhunkumarap@gmail.com>
2023-07-24 09:04:54 +0530

    Diameter of Binary Tree

--- Leet_Code_Python/Easy/Reverse_string_ll.py
+++ Leet_Code_Python/Easy/Reverse_string_ll.py
@@ -18,3 +18,45 @@ Output: "bacd"
 
 '''
 
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        temp = []
+        first = 0
+        last = k-1                 
+        while (first <= len(s)-1):     
+            if last > len(s)-1:
+                last = len(s) - 1
+            while(last >= first): 
+                temp.append(s[last])  
+                last = last - 1
+            first = first + k                 
+            last = first + (k-1)
+            while(last >= first):
+                if first > len(s)-1:
+                    break
+                temp.append(s[first])  
+                first = first + 1                
+            last = first + (k-1)       
+        return ''.join(temp)
+            
+#less time
+
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        p = 0
+        while p < len(s):
+            p2 = p+k
+            s = s[:p] + s[p:p2][::-1] + s[p2:]
+            p += 2*k
+        return s
+
+
+
+
+#less memory
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        a = list(s)
+        for i in range(0, len(a), 2*k):
+            a[i:i+k] = reversed(a[i:i+k])
+        return "".join(a) 
