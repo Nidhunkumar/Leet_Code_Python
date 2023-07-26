ced28cd6522bb794cc73b343ea97dc83a93cbff4
Nidhunkumar <nidhunkumarap@gmail.com>
2023-07-26 09:01:51 +0530

    Reverse Words in a String II

--- Leet_Code_Python/Easy/Student_attendance_record_l.py
+++ Leet_Code_Python/Easy/Student_attendance_record_l.py
@@ -26,3 +26,42 @@ Output: false
 Explanation: The student was late 3 consecutive days in the last 3 days, so is not eligible for the award.
 
 '''
+
+class Solution:
+    def checkRecord(self, s: str) -> bool:
+        A_count = 0
+        L_count = 0
+        for i in s:
+            if i == "A":
+                A_count += 1
+                if A_count == 2:
+                    return False
+            if i == "L":
+                L_count += 1
+                if L_count > 2:
+                    return False
+            else:
+                L_count = 0
+        return True
+    
+#less time
+
+class Solution:
+    def checkRecord(self, s: str) -> bool:
+        count = 0 
+        
+        for i in range(len(s)):
+            if s[i] == 'A':
+                count += 1 
+            
+            if i >= 2:
+                if s[i] == s[i-1] == s[i-2] == 'L':
+                    return False 
+        
+        return count < 2
+        
+#less memory
+
+class Solution:
+    def checkRecord(self, s: str) -> bool:
+        return not ("LLL" in s or s.count('A') > 1)
