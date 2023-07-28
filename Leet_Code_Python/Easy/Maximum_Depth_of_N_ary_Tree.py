3d1bac602b08a0ef9326114b18c09e88362f2db5
Nidhunkumar <nidhunkumarap@gmail.com>
2023-07-28 18:50:45 +0530

    Array Partition

--- Leet_Code_Python/Easy/Maximum_Depth_of_N_ary_Tree.py
+++ Leet_Code_Python/Easy/Maximum_Depth_of_N_ary_Tree.py
@@ -24,3 +24,72 @@ Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,1
 Output: 5
 
 '''
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if root == None:
+            return 0
+        
+        max_depth = 0
+        for child in root.children:
+            max_depth = max(max_depth, self.maxDepth(child))
+        
+        return 1 + max_depth
+    
+#less time
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+
+        def recursion(root, level):
+            nonlocal res
+            if not root.children:
+                res = max(res, level)
+                return
+            for child in root.children:
+                recursion(child, level + 1)
+            return
+        
+        if not root:
+            return 0
+
+        res = 0
+        recursion(root, 1)
+        return res
+
+#less memory
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        
+        temp = [0]
+        for c in root.children:
+            temp.append(self.maxDepth(c))
+        return max(temp)+1
+
