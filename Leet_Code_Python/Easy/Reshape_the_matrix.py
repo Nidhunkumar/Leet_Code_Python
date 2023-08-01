4ec619cae77e442a8bf912081093a5a09f2a7332
Nidhunkumar <nidhunkumarap@gmail.com>
2023-08-01 17:17:19 +0530

    Subtree of Another Tree

--- Leet_Code_Python/Easy/Reshape_the_matrix.py
+++ Leet_Code_Python/Easy/Reshape_the_matrix.py
@@ -23,3 +23,59 @@ Output: [[1,2],[3,4]]
 
 
 '''
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        if r*c != len(mat)* len(mat[0]):
+            return mat
+
+        final = []
+        temp = 0
+        c_temp = 0
+        row = []
+
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                if c_temp == c:
+                    final.append(list(row))
+                    row.clear()
+                    c_temp = 0
+
+                row.append(mat[i][j])
+                c_temp += 1
+        if row:
+            final.append(list(row))
+        return final
+        
+#less time
+
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        mat_flatten = list(chain(*mat))
+        if len(mat_flatten) / c != r:
+            return mat
+        result = []
+        for i in range(0, len(mat_flatten), c):
+            result.append(mat_flatten[i: i + c])
+        return result
+    
+#less memory
+
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        if r*c!=len(mat)*len(mat[0]):
+            return mat
+        ans=[[]]
+        
+        d1=len(mat)
+        d2=len(mat[0])
+        i1=0
+        for i in range(d1):
+            for j in range(d2):
+                ans[-1].append(mat[i][j])
+                i1+=1
+                if i1==c:
+                    ans.append([])
+                    i1=0
+        if ans[-1]==[]:
+            ans.pop()
+        return ans
