812ca5e2805a5abd32c744fdf1ea830fd3332a3e
Nidhunkumar <nidhunkumarap@gmail.com>
2023-08-17 18:19:58 +0530

    Design Compressed String Iterator

--- Leet_Code_Python/Easy/Minimum_index_sum_of_two_lists.py
+++ Leet_Code_Python/Easy/Minimum_index_sum_of_two_lists.py
@@ -32,4 +32,68 @@ Explanation: There are three common strings:
 The strings with the least index sum are "sad" and "happy".
 
 '''
+class Solution:
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        d={}
+        for i in range(len(list1)):
+            for j in range(len(list2)):
+                if list1[i] == list2[j]:
+                    d[list1[i]] = i+j
+        lst=[]
+        for i,j in d.items():
+            if j == min(d.values()):
+                lst.append(i)
+        return lst
 
+#less time
+
+class Solution:
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        dic1 = {}
+        for i, s in enumerate(list1):
+            if s not in dic1:
+                dic1[s] = i
+            
+        dic2 = {}
+        for i,s in enumerate(list2):
+            if s not in dic2:
+                dic2[s] = i
+        m = float('inf')
+        ans = []
+        for k,v in dic1.items():
+            if k in dic2:
+                p1 = v
+                p2 = dic2[k]
+                if m == p1 + p2:
+                    ans.append(k)
+                elif m > p1 + p2:
+                    m = p1 + p2
+                    ans = [k]
+        return ans
+
+#less memory
+
+class Solution:
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        
+        hashmap = dict()
+
+        if len(list1) < len(list2):
+            smallest = list1
+            biggest = list2
+        else:
+            smallest = list2
+            biggest = list1
+
+        for i in range(len(smallest)):
+
+            if smallest[i] in biggest:
+
+                try:
+                    hashmap[i + biggest.index(smallest[i])].append(smallest[i])
+
+                except KeyError:
+                    hashmap[i + biggest.index(smallest[i])] = [smallest[i]]
+
+        return hashmap[min(hashmap.keys())]
+    
