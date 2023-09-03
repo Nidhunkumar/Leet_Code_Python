d8ec8477bc43ee7bce6562a8f196e4e598a53df5
Nidhunkumar <nidhunkumarap@gmail.com>
2023-09-03 21:07:17 +0530

    Maximum Product of Three Numbers

--- Leet_Code_Python/Easy/Merge_Two_Binary_Tree.py
+++ Leet_Code_Python/Easy/Merge_Two_Binary_Tree.py
@@ -21,3 +21,84 @@ Example 2:
 Input: root1 = [1], root2 = [1,2]
 Output: [2,2]
 '''
+
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root1 is None and root2 is None:
+            return None
+        if root1 is None: return root2
+        if root2 is None: return root1
+        root1.val += root2.val
+        root1.left = self.mergeTrees(root1.left, root2.left)
+        root1.right = self.mergeTrees(root1.right, root2.right)
+        return root1
+
+
+#less time
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root1:
+            return root2
+        if not root2:
+            return root1
+
+        q = deque()
+        q.extend([root1, root2]) 
+
+        res = root1
+
+        while q:
+            root1,root2 = q.popleft(), q.popleft()
+
+            root1.val += root2.val
+
+            if root1.left and root2.left:
+                q.extend([root1.left, root2.left])
+            elif not root1.left:
+                root1.left = root2.left
+
+            if root1.right and root2.right:
+                q.extend([root1.right, root2.right])
+            elif not root1.right:
+                root1.right = root2.right
+
+        return res
+
+                
+
+
+#less memory
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        # preorder recursive dfs traversal
+
+        # if not root1 and not root2:
+        #     return None
+
+        if not root1:
+            return root2
+        
+        if not root2:
+            return root1
+
+        root1.val += root2.val
+        root1.left = self.mergeTrees(root1.left, root2.left)
+        root1.right = self.mergeTrees(root1.right, root2.right)
+        root2 = None
+
+        return root1
+
