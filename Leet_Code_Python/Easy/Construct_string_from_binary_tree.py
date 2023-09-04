283501d2eb11aef33c2b0e4fc4c6144cc1ce55cb
Nidhunkumar <nidhunkumarap@gmail.com>
2023-09-04 11:40:48 +0530

    Average of Levels in Binary Tree

--- Leet_Code_Python/Easy/Construct_string_from_binary_tree.py
+++ Leet_Code_Python/Easy/Construct_string_from_binary_tree.py
@@ -17,3 +17,102 @@ Output: "1(2()(4))(3)"
 Explanation: Almost the same as the first example, except we cannot omit the first parenthesis pair to break the one-to-one mapping relationship between the input and the output.
 
 '''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+        def tree2str(self, root: Optional[TreeNode]) -> str:
+                    
+            if(root == None):
+                return ""
+            
+            output: str = str(root.val)
+            
+            if (root.left != None or root.right != None):
+                output += "(" + self.tree2str(root.left) + ")"
+                
+            if (root.right != None):
+                output += "(" + self.tree2str(root.right) + ")"          
+            
+            return output;    
+
+#less time 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        def dfs(node, arr):
+            if not node:
+                return
+            if node:
+                arr.append(str(node.val))
+            if node.left:
+                arr.append('(')
+                dfs(node.left, arr)
+                arr.append(')')
+            if not node.left and node.right:
+                arr.append('()')
+            if node.right:
+                arr.append('(')
+                dfs(node.right, arr)
+                arr.append(')')
+        arr = []
+        dfs(root, arr)
+        return "".join(arr)
+    
+#less memory
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def _tree2str(self, root: Optional[TreeNode]) -> str:
+        return self.depth_first_pre_order(root)
+
+    def depth_first_pre_order(self, node: Optional[TreeNode]) -> str:
+        if not node.left and not node.right:
+            return str(node.val)
+
+        ans = ''
+        ans += '(' + self.depth_first_pre_order(node.left) + ')' if node.left else '()'
+        ans += '(' + self.depth_first_pre_order(node.right) + ')' if node.right else ''
+
+        return str(node.val) + ans
+
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        if not root:
+            return ''
+
+        stack = [root]
+        ans = ''
+
+        while stack:
+            node = stack[-1]
+
+            if hasattr(node, 'visited'):
+                stack.pop()
+                ans += ')'
+            else:
+                node.visited = True
+                ans += '(' + str(node.val)
+
+                if not node.left and node.right:
+                    ans += '()'
+                if node.right:
+                    stack.append(node.right)
+                if node.left:
+                    stack.append(node.left)
+
+        return ans[1:-1]
+    
--- Leet_Code_Python/Easy/Maximum_Product_of_Three_Numbers.py
+++ Leet_Code_Python/Easy/Maximum_Product_of_Three_Numbers.py
@@ -18,3 +18,61 @@ Output: -6
 
 
 '''
+
+class Solution:
+    def maximumProduct(self, vec: List[int]) -> int:
+        if len(vec) == 3:
+            return vec[0] * vec[1] * vec[2]
+        max1 = max2 = max3 = -1000 
+        min1 = min2 = 1000
+        for i in vec:
+            if i > max1:
+                max3 = max2
+                max2 = max1
+                max1 = i
+            elif i > max2:
+                max3 = max2
+                max2 = i
+            elif i > max3:
+                max3 = i
+            if i < min1:
+                min2 = min1
+                min1 = i
+            elif i < min2:
+                min2 = i
+        return max(max1 * max2 * max3, min1 * min2 * max1)
+    
+#less time
+
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        min1 = 1001
+        min2 = 1001
+        max1 = -1001
+        max2 = -1001
+        max3 = -1001
+        for num in nums:
+            if num <= min1:
+                min2 = min1
+                min1 = num
+            elif num <= min2:
+                min2 = num
+            if num >= max1:
+                max3 = max2
+                max2 = max1
+                max1 = num
+            elif num >= max2:
+                max3 = max2
+                max2 = num
+            elif num >= max3:
+                max3 = num
+        return max(max1 * max2 * max3, min2 * min1 * max1)
+    
+#less memory
+
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        return max(nums[n - 1] * nums[n - 2] * nums[n - 3], nums[0] * nums[1] * nums[n - 1])
+    
