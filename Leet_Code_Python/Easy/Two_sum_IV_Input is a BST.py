2a82f7b989ffcdbd4945447cf4212f70aaefdd89
Nidhunkumar <nidhunkumarap@gmail.com>
2023-09-07 21:57:03 +0530

    Robot Return to Origin

--- Leet_Code_Python/Easy/Two_sum_IV_Input is a BST.py
+++ Leet_Code_Python/Easy/Two_sum_IV_Input is a BST.py
@@ -14,3 +14,83 @@ Output: false
 
 
 '''
+
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        if not root:return False
+        visit=set()
+        q=[root]
+        for i in q:
+            if (k-i.val) in visit:return True
+            visit.add(i.val)
+            if i.left:
+                q.append((i.left))
+            if i.right:
+                q.append(i.right)
+        return False
+
+           
+#less time
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        vals = set()
+        def bfs(node):
+            if not node:
+                return False
+            if k-node.val in vals:
+                return True
+            vals.add(node.val)
+            return bfs(node.left) or bfs(node.right)
+        
+        return bfs(root)
+
+
+#less memory
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+"""
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        self.arr=[]
+        self.dfs(root)
+        comb=combinations(self.arr,2)
+        for i in comb:
+            if sum(i)==k: return True
+        return False
+    
+    def dfs(self,root):
+        if root is None: return
+        self.arr.append(root.val)
+        self.dfs(root.left)
+        self.dfs(root.right)
+"""
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        visited=[]
+        queue=[root]
+        while queue:
+            node=queue.pop()
+            findNum=k-node.val
+            if findNum in visited: return True
+            else: visited.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right: 
+                queue.append(node.right)
+        return False
+
+
+            
