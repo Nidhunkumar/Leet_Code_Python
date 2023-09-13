a37890c92287ad5d8e5ed5041389ae184d3bc1f3
Nidhunkumar <nidhunkumarap@gmail.com>
2023-09-13 15:03:18 +0530

    Longest Continuous Increasing Subsequence

--- Leet_Code_Python/Easy/Second_minimum_Node_In_Binary_Tree.py
+++ Leet_Code_Python/Easy/Second_minimum_Node_In_Binary_Tree.py
@@ -24,3 +24,69 @@ Output: -1
 Explanation: The smallest value is 2, but there isn't any second smallest value.
 
 '''
+class Solution:
+    def findSecondMinimumValue(self, root: Optional[TreeNode]) -> int:
+        a = []
+
+        def dfs(root):
+            if not root:
+                return
+            dfs(root.left)
+            a.append(root.val)
+            dfs(root.right)
+
+        dfs(root)
+
+        return (sorted(set(a)))[1] if len(set(a)) >= 2 else -1
+    
+#less time 
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findSecondMinimumValue(self, root: Optional[TreeNode]) -> int:
+        s=set()
+        def helper(root,s):
+            if(root==None):
+                return 
+            helper(root.left,s)
+            s.add(root.val)
+            helper(root.right,s)
+        helper(root,s)
+        s=list(s)
+        s.sort()
+        return s[1] if len(s)>1 else -1
+
+#less memory
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findSecondMinimumValue(self, root: Optional[TreeNode]) -> int:
+        ans = -1
+        def dfs(node, v):
+            nonlocal ans
+
+            if not node:
+                return
+
+            if node.val > v:
+                if ans == -1:
+                    ans = node.val
+                else:
+                    ans = min(ans, node.val)
+                return
+
+            dfs(node.left, v)
+            dfs(node.right, v)
+
+        dfs(root, root.val)
+        return ans
